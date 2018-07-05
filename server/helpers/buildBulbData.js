07b50ca67a759e5fc91e4ddabdcfc14389a38ac8
joshborup <joshborup@gmail.com>
2018-07-04 21:55:25 -0700

    updated data generation function

--- server/helpers/buildBulbData.js
+++ server/helpers/buildBulbData.js
@@ -2,12 +2,13 @@ let colors = ['red','yellow', 'green', 'blue', 'orange']
 
 
 function getData(num){
+    let letters = ('abcdefghijklmnopqrstuvwxyz').toUpperCase()
     let data = [];
-    for(let i = 1; i < num; i++){
+    for(let i = 0; i < num; i++){
         let rando = Math.random() * ((colors.length + 1) - 1);
-        data.push({id: i, color: colors[Math.floor(rando)]})
+        data.push({id: i, color: colors[Math.floor(rando)], letter: letters[i]})
     }
     return data;
 }
 
-getData(23);
+getData(26);
